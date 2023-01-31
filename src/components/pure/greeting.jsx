24d541669177e00518ceccafb2cfb4ff37ad4154
Giovanny Badilo <giovannybm@gmail.com>
2023-01-30 19:40:18 -0500

    ident fix

--- src/components/pure/greeting.jsx
+++ src/components/pure/greeting.jsx
@@ -2,32 +2,32 @@ import React, { Component } from "react";
 import PropTypes from "prop-types"
 
 class Greeting extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            age: 29
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      age: 29
     }
-    birthday=()=>{
-        this.setState((prevState)=>( {
-            age:prevState.age +1
-        })
+  }
+  birthday = () => {
+    this.setState((prevState) => ({
+      age: prevState.age + 1
+    })
 
-        )
-    }
-    render() {
-        return (
-            <div>
-                <h1>Hola {this.props.name}</h1>
-                <h2>Tu edad es: {this.state.age}</h2>
-                <button onClick={this.birthday}>Increment Age</button>
-            </div>
-        )
-    }
+    )
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hola {this.props.name}</h1>
+        <h2>Tu edad es: {this.state.age}</h2>
+        <button onClick={this.birthday}>Increment Age</button>
+      </div>
+    )
+  }
 }
 
 Greeting.propTypes = {
-    name: PropTypes.string,
+  name: PropTypes.string,
 }
 
 export default Greeting
--- src/components/pure/hello.jsx
+++ src/components/pure/hello.jsx
@@ -2,21 +2,21 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 const Hello = ({ name }) => {
-    const [age, setAge] = useState(29)
-    const birthday = () => {
-        setAge(age + 1)
-    }
-    return (
-        <div>
-            <h1>Hola {name} desde componente Funcional</h1>
-            <h2>Tu edad es: {age}</h2>
-            <button onClick={birthday}>Increment Age</button>
-        </div>
-    )
+  const [age, setAge] = useState(29)
+  const birthday = () => {
+    setAge(age + 1)
+  }
+  return (
+    <div>
+      <h1>Hola {name} desde componente Funcional</h1>
+      <h2>Tu edad es: {age}</h2>
+      <button onClick={birthday}>Increment Age</button>
+    </div>
+  )
 }
 
 Hello.propTypes = {
-    name: PropTypes.string,
+  name: PropTypes.string,
 }
 
 export default Hello
